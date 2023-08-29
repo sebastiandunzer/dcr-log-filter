7f88441307fbaff6591b6d96bb6aabd8a6e4f13a
ho87nyze <sebastian.dunzer@fau.de>
2023-08-29 09:43:04 +0200

    Conducted further experiments

--- transpiler/cmd_parser.py
+++ transpiler/cmd_parser.py
@@ -11,14 +11,14 @@ def parse_args():
     """
     # This file contains the commandline tools for the current tool
     parser = argparse.ArgumentParser(prog='main.py', usage='main.py [options]')
-    parser.add_argument('--eventLog', nargs='?', default='Resources/BPI2019.xes',
+    parser.add_argument('--eventLog', nargs='?', default='Resources/Hospital_Billing.xes',
                         help='The path pulling the event log')
-    parser.add_argument('--XmlDcr', nargs="?", default='Resources/BPI2019_loose.xml',
+    parser.add_argument('--XmlDcr', nargs="?", default='Resources/Hospital_Billing.xml',
                         help='The input path for the DCR Graph xml')
     parser.add_argument('--output', nargs='?')
     parser.add_argument('--namespace', nargs='?', default='')
-    parser.add_argument('--outFile', nargs='?', default="Resources/BPI2019_loose_prep.xes")
-    parser.add_argument('--importModel', nargs='?', default="")
-    parser.add_argument('--minerName',nargs='?', default="")
+    parser.add_argument('--outFile', nargs='?', default="Resources/Hospital_Billing_prep.xes")
+    parser.add_argument('--importModel', nargs='?', default="Resources/Hospital_Billing_Filtered.pnml")
+    parser.add_argument('--minerName',nargs='?', default="trace")
 
     return parser.parse_args()
